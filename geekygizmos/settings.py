1876adc8108f6a7b203cee743334fdaa7bb39fe0
DavidcD8 <>
2023-11-13 17:28:17 +0000

    add aws code to settings and custome storage page

--- geekygizmos/settings.py
+++ geekygizmos/settings.py
@@ -46,9 +46,10 @@ INSTALLED_APPS = [
     'checkout',
     'onlineshop',
     'crispy_forms',
-    'whitenoise'
+    'whitenoise',
+    'storages',
     
-
+ 
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -150,6 +151,19 @@ USE_I18N = True
 USE_TZ = True
 
 
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'onlineshopfirst'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -167,6 +181,10 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'gg@example.com'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  

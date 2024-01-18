59252244ff1e5e80527e81587d43634c84107759
DavidcD8 <>
2024-01-18 15:41:16 +0000

    changed back AWS static storage

--- geekygizmos/settings.py
+++ geekygizmos/settings.py
@@ -179,8 +179,6 @@ if "USE_AWS" in os.environ:
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
-
-
 WHITENOISE_USE_FINDERS = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
